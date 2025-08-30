728817ff9c98173edcb3d82a3da88bc737ae57b2
kanishq-9 <purohitkanishq9@gmail.com>
2025-08-30 14:07:17 +0530

    initial document

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+
+app.use(cors);
+app.use(helmet());
+app.use(express.json());
+
 
 app.get('/',(req,res)=>{
     res.send("got response");
