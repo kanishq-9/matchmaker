f3db1631c3fefce75bd15614a48c1c225420a052
kanishq-9 <purohitkanishq9@gmail.com>
2025-09-01 00:33:45 +0530

    added login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { GoPerson, GoLock } from "react-icons/go";
+
+import "./css/login.css";
 
 export default function Login() {
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log("form submitted");
+    
+  }
+
+
   return (
-    <div>Login</div>
+    <div className='login p-2 rounded d-flex flex-column align-items-center'>
+      <div className='display-4'>Login</div>
+      <form onSubmit={handleSubmit} className='m-2 d-flex flex-column h5'>
+        <span>
+          <GoPerson/>
+        <input className='m-2 border-0' type="text" placeholder="Username"/>
+        </span>
+        <span>
+          <GoLock />
+    <input className='m-2 border-0' type="password" placeholder='password'/>
+        </span>
+        
+        
+
+        <button className='btn login-btn m-2' type='submit'>Login</button>
+
+      </form>
+    </div>
+
   )
 }
---
+++ server/firebaseimport.js
@@ -0,0 +1,30 @@
+const admin = require('firebase-admin');
+const fs = require('fs');
+
+const serviceAccount = require('./config/secret/matchmaker-d42d8-firebase-adminsdk-fbsvc-da9489f3aa.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const data = JSON.parse(fs.readFileSync("../docs/matrimonial_dataset_firestore.json","utf-8"));
+async function importData() {
+    const users = data.users;
+    for(const key in users){
+        const user = users[key];
+        await db.collection("users").doc(key).set(user);
+        console.log(`imported ${key}`);
+        
+    }
+}
+
+importData()
+.then(()=>{
+    console.log("import complete");
+    process.exit();
+})
+.catch(err=>{
+    console.error("❌ Error importing data:", err);
+    process.exit(1);
+})
