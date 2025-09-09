32174b8968bd8309c460bb19b558e71bef79ef04
kanishq-9 <purohitkanishq9@gmail.com>
2025-09-09 16:17:42 +0530

    Added security

--- server/stagesimport.js
+++ server/stagesimport.js
@@ -1,7 +1,22 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 
-const serviceAccount = require('./config/secret/matchmaker-d42d8-firebase-adminsdk-fbsvc-da9489f3aa.json');
+import dotenv from "dotenv";
+dotenv.config();
+
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
